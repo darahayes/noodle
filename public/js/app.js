2c966ebc83f48b2489217078171a926a7497c28e
Darragh Hayes <dara.hayes.1995@gmail.com>
2016-02-19 02:56:57 +0000

    boilerplate for nested views

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,40 @@
 function configure_states($stateProvider, $urlRouterProvider) {
-	$stateProvider
+  $stateProvider
 
-	.state('home', {
-		url: '/home',
-		templateUrl: 'templates/home.html',
-		controller: function($scope) {
-        $scope.items = ["B", "List", "Of", "Items"];
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/app.html',
+    controller: function($scope) {
+      $scope.title = 'Noodle'
+    }
+  })
+
+  .state('app.state0', {
+    url: '/home',
+    views: {
+      viewContent: {
+        templateUrl: 'templates/view0.html',
+        controller: function($scope) {
+          $scope.items = ["B", "List", "Of", "Items"];
+        }
       }
-	})
+    }
+  })
 
-	$urlRouterProvider.otherwise('/home');
+  .state('app.state1', {
+    url: '/home2',
+    views: {
+      viewContent: {
+        templateUrl: 'templates/view1.html',
+        controller: function($scope) {
+          $scope.items = ["this", "is", "a", "different", "view"];
+        }
+      }
+    }
+  })
+
+  $urlRouterProvider.otherwise('/app/home');
 }
 
 
